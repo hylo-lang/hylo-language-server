180e686a001c3a8dec08caedcc6d45125fda48d0
Ambrus Toth <ping@ambrus.dev>
2025-12-06 20:40:52 +0000

    Adopting the new vscode plugin

--- build-and-install-vscode-extension.sh
+++ build-and-install-vscode-extension.sh
@@ -4,7 +4,7 @@ set -ex
 
 swift build -c release
 BUILD_DIR=$(swift build -c release --show-bin-path)
-DIST_DIR=hylo-vscode-extension/dist
+DIST_DIR=vscode-hylo/dist
 rm -rf $DIST_DIR
 mkdir -p $DIST_DIR
 cp -Rp hylo-new/StandardLibrary/Sources $DIST_DIR/hylo-stdlib
@@ -12,8 +12,8 @@ mkdir -p $DIST_DIR/bin/
 cp -fv $BUILD_DIR/hylo-lsp-server $DIST_DIR/bin/
 PUBLISHED_AT=$(date -u '+%Y-%m-%dT%H:%M:%SZ')
 echo "{\"name\": \"dev\", \"id\": 0, \"published_at\": \"$PUBLISHED_AT\"}" > $DIST_DIR/manifest.json
-cd hylo-vscode-extension
+cd vscode-hylo
 npm install
-npm run vscode:package
+npm run package:vsix
 VERSION=$(cat package.json | jq -r ".version")
-code --install-extension hylo-lang-$VERSION.vsix
+code --install-extension hylo-vscode-$VERSION.vsix
