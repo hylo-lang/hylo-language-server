d3de7fe79268cb6b378c3a462b0e8030805c8d77
Ambrus Toth <ping@ambrus.dev>
2026-01-25 20:31:57 +0000

    Improved renaming

--- Sources/HyloLanguageServerCore/Features/Rename.swift
+++ Sources/HyloLanguageServerCore/Features/Rename.swift
@@ -36,19 +36,28 @@ extension HyloRequestHandler {
       }
 
       if let name = doc.program.cast(node, to: NameExpression.self) {
+        // If no target declaration, cannot be renamed.
+        guard let referredDeclaration = doc.program.declaration(referredToBy: name).target else {
+          return .success(nil)
+        }
+        // Cannot rename initializers.
+        if let f = doc.program.cast(referredDeclaration, to: FunctionDeclaration.self) {
+          if doc.program[f].introducer.value != .fun { return .success(nil) }
+        }
+        // Cannot rename `self` parameter
+        if let p = doc.program.cast(referredDeclaration, to: ParameterDeclaration.self) {
+          if doc.program[p].identifier.value == "self" { return .success(nil) }
+        }
+
         return .success(.optionA(LSPRange(doc.program[name].name.site)))
       }
 
-      if let variableDeclaration = doc.program.cast(node, to: VariableDeclaration.self) {
-        return .success(
-          .optionA(LSPRange(doc.program[variableDeclaration].identifier.site)))
+      if let declaration = doc.program.castToDeclaration(node),
+        let identifier = doc.program.identifier(of: declaration)
+      {
+        return .success(.optionA(LSPRange(identifier.site)))
       }
-      //todo
-      // if let parameterDeclaration = doc.program.cast(
-      //   node, to: ParameterDeclaration.self)
-      // {
-      //   return .success(.optionA(LSPRange(doc.program[parameterDeclaration].identifier.site)))
-      // }
+
       return .failure(
         .init(code: ErrorCodes.InvalidParams, message: "Cannot rename symbol at cursor."))
     }
@@ -90,20 +99,92 @@ extension HyloRequestHandler {
               message: "No target to rename for related declaration."))
         }
 
-        var references = findReferences(of: declarationToRename, in: doc.program)
-        references.append(doc.program.spanForDiagnostic(about: declarationToRename))  // todo be smarter, only rename stuff with identifiers
+        return .success(
+          workspaceEditsForRenaming(
+            declaration: declarationToRename,
+            to: params.newName,
+            in: doc.program))
+      }
 
-        let changes: [DocumentUri: [TextEdit]] = [
-          params.textDocument.uri: references.map { site in
-            TextEdit(
-              range: LSPRange(site),
-              newText: params.newName)
-          }
-        ]
-
-        return .success(WorkspaceEdit(changes: changes, documentChanges: nil))
+      if let declaration = doc.program.castToDeclaration(node) {
+        return .success(
+          workspaceEditsForRenaming(
+            declaration: declaration,
+            to: params.newName,
+            in: doc.program))
       }
       return .success(nil)
     }
+  }
+
+  func workspaceEditsForRenaming(
+    declaration: DeclarationIdentity, to newName: String, in program: Program
+  )
+    -> WorkspaceEdit?
+  {
+    guard let identifier = program.identifier(of: declaration) else {
+      return nil
+    }
+
+    var spansToChange = findReferences(of: declaration, in: program)
+    spansToChange.append(identifier.site)
+
+    return workspaceEdits(renaming: spansToChange, to: newName)
+  }
+}
+
+func workspaceEdits(renaming: [SourceSpan], to: String) -> WorkspaceEdit {
+  var changes: [DocumentUri: [TextEdit]] = [:]
+  for span in renaming {
+    let uri = DocumentUri(span.source.name.absoluteUrl!.url.absoluteString)
+    let edit = TextEdit(
+      range: LSPRange(span),
+      newText: to)
+    changes[uri, default: []].append(edit)
+  }
+  return WorkspaceEdit(changes: changes, documentChanges: nil)
+}
+
+extension Program {
+  func identifier(of declaration: DeclarationIdentity) -> Parsed<String>? {
+    if let associatedTypeDeclaration = cast(declaration, to: AssociatedTypeDeclaration.self) {
+      return self[associatedTypeDeclaration].identifier
+    } else if cast(declaration, to: BindingDeclaration.self) != nil {
+      return nil
+    } else if let conformanceDeclaration = cast(declaration, to: ConformanceDeclaration.self) {
+      return self[conformanceDeclaration].identifier
+    } else if let enumCaseDeclaration = cast(declaration, to: EnumCaseDeclaration.self) {
+      return self[enumCaseDeclaration].identifier
+    } else if let enumDeclaration = cast(declaration, to: EnumDeclaration.self) {
+      return self[enumDeclaration].identifier
+    } else if cast(declaration, to: ExtensionDeclaration.self) != nil {
+      return nil
+    } else if let FunctionBundleDeclaration = cast(declaration, to: FunctionBundleDeclaration.self)
+    {
+      return self[FunctionBundleDeclaration].identifier
+    } else if let functionDeclaration = cast(declaration, to: FunctionDeclaration.self),
+      case .simple(let simpleIdentifier) = self[functionDeclaration].identifier.value  // todo rename operators?
+    {
+      return Parsed<String>(simpleIdentifier, at: self[functionDeclaration].identifier.site)
+    } else if let genericParameterDeclaration = cast(
+      declaration, to: GenericParameterDeclaration.self)
+    {
+      return self[genericParameterDeclaration].identifier
+    } else if cast(declaration, to: ImportDeclaration.self) != nil {
+      return nil
+    } else if let parameterDeclaration = cast(declaration, to: ParameterDeclaration.self) {
+      return self[parameterDeclaration].identifier
+      // todo argument labels -> rename the call sites
+    } else if let structDeclaration = cast(declaration, to: StructDeclaration.self) {
+      return self[structDeclaration].identifier
+    } else if let traitDeclaration = cast(declaration, to: TraitDeclaration.self) {
+      return self[traitDeclaration].identifier
+    } else if let typeAliasDeclaration = cast(declaration, to: TypeAliasDeclaration.self) {
+      return self[typeAliasDeclaration].identifier
+    } else if let variableDeclaration = cast(declaration, to: VariableDeclaration.self) {
+      return self[variableDeclaration].identifier
+    }
+
+    return nil
   }
 }
