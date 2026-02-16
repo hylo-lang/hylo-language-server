bd030897d3b6d6256ec75953f7bca3c93c9eac0e
MartTave <55973969+MartTave@users.noreply.github.com>
2026-02-16 10:43:20 +0100

    typo

---
+++ Sources/HyloLanguageServerCore/Features/Test.swift
@@ -0,0 +1,16 @@
+class Test {
+    public init (_ a:Int = 2, b:Int) {
+
+    }
+
+    public func f1(a: Int) {
+
+    }
+    public func f1(a: Bool) {
+
+    }
+}
+
+public func main() {
+    let t = Test(b: 2)
+}
